09753690a978385426bf366a34a54e62cbbb60f5
AKama06 <Abioseh.Kamara@uea.ac.uk>
2023-02-17 00:25:27 +0000

    Created linear model comparing mean heights of selfed and crossed plants from the darwin data. Summarised the data of the model. Grouped Darwin data by type and summarised mean heights.

--- 1010022023_linear_model_analysis.R
+++ 1010022023_linear_model_analysis.R
@@ -4,3 +4,19 @@ library(tidyverse)
 library(GGally)
 library(emmeans)
 library(performance)
+
+#__________________----
+#VISUALISATION----
+lsmodel0 <- lm(formula = height ~ 1, data = darwin)
+summary(lsmodel0)
+
+#mean of height
+mean(darwin$height)
+#COMPARING MEAN====
+lsmodel1 <- lm(height ~ type, data=darwin)
+#summarizes information about model components
+broom::tidy(lsmodel1)
+
+darwin %>% 
+  group_by(type) %>% 
+  summarise(mean=mean(height))
