f3a82a09f92502dc56d384c77662f2e741aeff7e
AKama06 <Abioseh.Kamara@uea.ac.uk>
2023-02-24 13:52:03 +0000

    Added packages Imported data Clean data Plotted graph

--- 24022023_regression.R
+++ 24022023_regression.R
@@ -3,3 +3,19 @@
 library(tidyverse)
 library(rstatix)
 library(performance)
+library(here)
+library(janitor)
+
+#_____________----
+#ACTIVITY 1 ----
+#import data
+janka <- read_csv(here("data", "janka.csv"))
+#clean data
+head(janka)
+janka <- janitor::clean_names(janka)
+glimpse(janka)
+summary(is.na(janka))#check for any NA values
+janka %>%
+  ggplot(aes(x=dens,
+       y=hardness))+
+  geom_point()
