05c3ba3714ff2c27700fe0360899cc9b396cc936
AKama06 <Abioseh.Kamara@uea.ac.uk>
2023-03-03 00:19:25 +0000

    Pearson's r test on janka data. Regression linear model and linear model plot.

--- 24022023_regression.R
+++ 24022023_regression.R
@@ -10,12 +10,38 @@ library(janitor)
 #ACTIVITY 1 ----
 #import data
 janka <- read_csv(here("data", "janka.csv"))
+
 #clean data
 head(janka)
 janka <- janitor::clean_names(janka)
 glimpse(janka)
 summary(is.na(janka))#check for any NA values
+
+#plot
 janka %>%
   ggplot(aes(x=dens,
        y=hardness))+
   geom_point()
+
+#pearson's r
+janka_correlation <-
+  cor.test(x= janka$dens, y= janka$hardness, method = "pearson")
+janka_correlation
+
+#person's r using rstatix package
+#base r
+with(janka, cor(dens, hardness))
+#tidyverse
+janka %>% 
+  cor_test(dens, hardness)
+
+#regression model ----
+janka_ls1 <- lm(hardness ~ dens, data = janka) 
+janka_ls1
+
+#linear model plot
+janka %>% 
+  ggplot(aes(x=dens, y=hardness))+
+  geom_point()+
+  geom_smooth(method="lm")
+# specify linear model method for line fitting
